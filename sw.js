60a0d8120234520c836236717bd44f6757d4bf6f
Mahesh13247 <139057471+Mahesh13247@users.noreply.github.com>
2025-05-28 15:04:22 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-restricted-globals */
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+
+precacheAndRoute(self.__WB_MANIFEST || []);
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new StaleWhileRevalidate()
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
